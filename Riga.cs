06dd0c543bc22e256e48fde01e8e627beeb3b276
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-03-07 23:30:04 +0100

    Add files via upload

---
+++ Riga.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1
+{
+    public class Riga
+    {
+        private List<Cella> celle = new List<Cella>();
+        public Riga() { }
+
+        public Cella CreaCella()
+        {
+            Cella c = new Cella();
+            celle.Add(c);
+            return c;
+        }
+        public override string ToString()
+        {
+            string riga = "<td>\n";
+            foreach (Cella c in celle)
+            {
+                riga = riga + "testo scritto...";
+            }
+            return riga + "\n<td>";
+        }
+
+    }
+}
---
+++ Tabella.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1
+{
+    public class Tabella : IRigaCreatore
+    {
+        private List<Riga> righe = new List<Riga>();
+        private static Tabella instanza { get; set; } = new Tabella();
+
+        internal Tabella() { }
+
+        public Tabella GetTabella()
+        {
+            if(instanza == null)
+            {
+                instanza = new Tabella();
+            }
+            return instanza;
+        }
+        public Riga CreatoreRiga()
+        {
+            Riga r = new Riga();
+            righe.Add(r);
+            return r;
+        }
+
+        public override string ToString()
+        {
+            string tabella = "<tabella>\n";
+            foreach(Riga r in righe)
+            {
+                tabella = tabella + r.ToString();
+            }
+            return tabella + "\n<tabella>";
+        }
+
+        
+    }
+}
